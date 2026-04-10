ce099d74e943eacdc2c65a89991295bb403b4924
bunnysammy_ <davidenacrene@gmail.com>
2026-04-10 16:34:05 -0300

    :lipstick: feat: Melhorias na estilização

--- scripts/map.js
+++ scripts/map.js
@@ -1,18 +1,6 @@
 import { player } from "./player.js";
-import { castRay } from './raycaster.js';
-
-const mapString = `
-1,1,1,1,1,1,1,1,1,1
-1,0,1,0,0,0,0,0,0,1
-1,0,1,0,0,0,0,0,0,1
-1,0,1,1,1,0,0,1,0,1
-1,0,0,0,0,0,0,0,0,1
-1,1,1,1,0,1,0,0,0,1
-1,0,0,0,0,1,0,0,0,1
-1,0,1,0,0,1,0,0,0,1
-1,0,1,0,0,1,0,0,0,1
-1,1,1,1,1,1,1,1,1,1
-`;
+import { castRay } from "./raycaster.js";
+import { mapString, colors } from "./utils.js"
 
 // Separa a string acima em uma matriz de inteiros
 export const map = mapString.trim().split("\n").map(row => row.split(",").map(Number));
@@ -29,9 +17,9 @@ export function drawMinimap (ctx) {
     for (let y = 0; y < map.length; y++) {
         for (let x = 0; x < map[y].length; x++) {
             if (map[y][x] > 0) {
-                ctx.fillStyle = "#FFF";
+                ctx.fillStyle = colors.foreground;
             } else {
-                ctx.fillStyle = "#000";
+                ctx.fillStyle = colors.background;
             }
 
             ctx.fillRect(
@@ -49,7 +37,7 @@ export function drawPlayer (ctx) {
     const px = (player.x * tileSize);
     const py = (player.y * tileSize);
 
-    ctx.fillStyle = "#c2fc03";
+    ctx.fillStyle = colors.player;
     ctx.beginPath();
     ctx.arc(px, py, 3, 0, Math.PI * 2);
     ctx.fill();
@@ -58,10 +46,11 @@ export function drawPlayer (ctx) {
 // Traça os raios que compõem o campo de visão do jogador
 export function drawRays (ctx2d, ctx3d) {
     clearCanvas(ctx3d);
+
     const px = (player.x * tileSize);
     const py = (player.y * tileSize);
     const fov = Math.PI / 3;
-    const rayCount = 120;
+    const rayCount = 60;
 
     // Dentro do campo de visão (60°) são traçados 60 raios
     for (let i = 0; i < rayCount; i++) {
@@ -76,7 +65,7 @@ export function drawRays (ctx2d, ctx3d) {
 
 // Desenha um raio no minimapa
 function drawRay2D (ctx, px, py, ray, angle) {
-    ctx.strokeStyle = "#c2fc0344";
+    ctx.strokeStyle = colors.player + colors.rayAlpha;
     ctx.beginPath();
     ctx.moveTo(px, py);
     ctx.lineTo(
@@ -95,8 +84,8 @@ function drawRay3D (ctx, i, ray, rayAngle, rayCount) {
     const lineHeight = Math.min(maxHeight / distance, maxHeight);
 
     // Escurece linhas mais distantes
-    const shade = Math.max(0, 255 - distance * 20);
-    ctx.fillStyle = `rgb(${shade}, ${shade}, ${shade})`;
+    const shade = Math.max(0, 255 - distance * 25);
+    ctx.fillStyle = `rgb(${shade}, ${shade}, ${shade+25})`;
     ctx.fillRect(
         i * increment,
         (maxHeight / 2) - (lineHeight / 2),
